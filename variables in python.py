ccb123c59aa3964c0e2633239251d35a0176a59f
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-08-23 11:35:42 +0500

    Add files via upload

---
+++ variables in python.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x, y, z="sharps" , "brain" ,"sharpsbrain"
+print(x)
+print(y)
+print(z)
+
+
+# In[2]:
+
+
+x=y=z="sharpsbrain"
+print(x)
+print(y)
+print(z)
+
+
+# In[3]:
+
+
+fruits=["apple","banana","cherry"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+
+# In[6]:
+
+
+x, y, z="sharps" , "brain" ,"sharpsbrain"
+print(x + y + z)
+
+
+# In[7]:
+
+
+x=5
+y="sharps"
+print(x+y)
+
+
+# In[9]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
